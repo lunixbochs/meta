3d55891f6304d1f6b515d7dfdc25ea27ba42baa6
Ryan Hileman <lunixbochs@gmail.com>
2025-10-17 23:26:56 -0700

    snakedisk: refactor devices, add resume support

--- utils/snakedisk/snakedisk.py
+++ utils/snakedisk/snakedisk.py
@@ -5,123 +5,182 @@
 # ]
 
 from concurrent.futures import ThreadPoolExecutor
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
+from pathlib import Path
 from shishua import SHISHUA
 from tqdm import tqdm
 import argparse
+import functools
+import json
 import os
 import random
 import sys
 import time
+import traceback
+
+@dataclass
+class Device:
+    path: str
+    size: int
+    limit: int
+    read: int
+    wrote: int
+    errors: int
+    seed: bytes
+    state: str # reading | writing | done
 
 @dataclass
 class State:
     pbar: tqdm
     blocksize: int
     limit: int
+    devices: dict[str, Device]
+    save_path: Path | None
 
-    bytes_written: int = 0
-    bytes_read: int = 0
-    bytes_total: int = 0
+def wrap_exc[T](fn: Callable[T]) -> Callable[T]:
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except Exception:
+            traceback.print_exc()
+            raise
+    return wrapper
 
-    drives_reading: int = 0
-    drives_writing: int = 0
-    drives_done: int = 0
-    drives_total: int = 0
-    errors: int = 0
-    last_tick: float = 0.0
-
-    def refresh(self):
-        now = time.perf_counter()
-        if now - self.last_tick < 0.032:
-            return
-        self.last_tick = now
-
-        self.pbar.n = self.bytes_written + self.bytes_read
-        self.pbar.total = self.bytes_total
-        self.pbar.set_postfix({
-            "drives": f"{self.drives_writing}w+{self.drives_reading}r+{self.drives_done}/{self.drives_total}",
-            "errors": self.errors,
-        })
-        self.pbar.refresh()
-
-def device_init(device: str, state: State):
-    state.drives_total += 1
-    with open(device, "rb") as f:
+@wrap_exc
+def device_init(path: str, state: State):
+    with open(path, "rb") as f:
         size = f.seek(0, os.SEEK_END)
     limit = size if not state.limit else min(size, state.limit)
-    state.bytes_total += limit * 2
+    seed = path + "|" + os.urandom(8).hex()
 
-def device_work(device: str, state: State):
+    if path in state.devices:
+        assert state.devices[path].size == size
+    else:
+        state.devices[path] = Device(
+            path=path,
+            size=size,
+            limit=limit,
+            read=0,
+            wrote=0,
+            errors=0,
+            seed=seed,
+            state="none",
+        )
+
+@wrap_exc
+def device_work(device: Device, state: State) -> None:
     blocksize = state.blocksize
 
-    seed = device + "|" + os.urandom(8).hex()
-    rng = SHISHUA(seed)
-    fd = os.open(device, os.O_WRONLY | os.O_DIRECT)
+    rng = SHISHUA(device.seed)
+    fd = os.open(device.path, os.O_WRONLY | os.O_DIRECT)
     with os.fdopen(fd, "wb", buffering=-1) as f:
         size = f.seek(0, os.SEEK_END)
-        f.seek(0, os.SEEK_SET)
-        limit = size if not state.limit else min(size, state.limit)
-        state.drives_writing += 1
+        device.state = "writing"
 
-        count = 0
-        for i in range(0, limit, blocksize):
-            x = rng.random_raw(blocksize)
-            n = f.write(x)
-            count += n
-            state.bytes_written += n
-            state.refresh()
-        if count < limit:
-            print(f"WARNING: {device} wrote short ({n})", file=sys.stderr)
-        state.drives_writing -= 1
-
-    rng = SHISHUA(seed)
-    with open(device, "rb") as f:
-        size = f.seek(0, os.SEEK_END)
-        f.seek(0, os.SEEK_SET)
-        state.drives_reading += 1
-
-        count = 0
+        device.wrote -= device.wrote % blocksize
         buf = bytearray(blocksize)
-        for i in range(0, limit, blocksize):
+        for i in range(0, device.wrote, blocksize):
+            rng.fill(buf)
+
+        f.seek(device.wrote, os.SEEK_SET)
+        for i in range(device.wrote, device.limit, blocksize):
+            rng.fill(buf)
+            n = f.write(buf)
+            device.wrote += n
+        if device.wrote < device.limit:
+            print(f"WARNING: {device.path} wrote short ({device.wrote} < {device.limit})", file=sys.stderr)
+
+    rng = SHISHUA(device.seed)
+    with open(device.path, "rb") as f:
+        size = f.seek(0, os.SEEK_END)
+        device.state = "reading"
+
+        buf = bytearray(blocksize)
+        device.read -= device.read % blocksize
+        for i in range(0, device.read, blocksize):
+            rng.fill(buf)
+
+        f.seek(device.read, os.SEEK_SET)
+        for i in range(device.read, device.limit, blocksize):
             n = f.readinto(buf)
             x = rng.random_raw(blocksize)
             if buf[:n] != x[:n]:
-                state.errors += 1
-                print(f"ERROR: {device} verify mismatch at {i}", file=sys.stderr)
+                device.errors += 1
+                print(f"ERROR: {device.path} verify mismatch at {i}", file=sys.stderr)
+            device.read += n
+        if device.read < device.limit:
+            print(f"WARNING: {device.path} read short ({device.read} < {device.limit})", file=sys.stderr)
+    device.state = "done"
 
-            count += n
-            state.bytes_read += n
-            state.refresh()
-        if count < limit:
-            print(f"WARNING: {device} read short ({n})", file=sys.stderr)
-        state.drives_reading -= 1
-    state.drives_done += 1
-    state.refresh()
+@wrap_exc
+def refresh_loop(state: State) -> None:
+    def save():
+        j = {"devices": {path: asdict(d) for path, d in state.devices.items()}}
+        with open(state.save_path, "w") as f:
+            json.dump(j, f)
+
+    last_save = time.perf_counter()
+    while True:
+        now = time.perf_counter()
+        if state.save_path and now - last_save > 60.0:
+            save()
+            last_save = now
+
+        devices = state.devices.values()
+        state.pbar.n = sum(dev.read + dev.wrote for dev in devices)
+        state.pbar.total = sum(dev.limit for dev in devices) * 2
+        drives_reading = sum(dev.state == "reading" for dev in devices)
+        drives_writing = sum(dev.state == "writing" for dev in devices)
+        drives_done = sum(dev.state == "done" for dev in devices)
+        state.pbar.set_postfix({
+            "drives": f"{drives_writing}w+{drives_reading}r+{drives_done}/{len(state.devices)}",
+            "errors": sum(dev.errors for dev in devices),
+        })
+        state.pbar.refresh()
+        if drives_done == len(state.devices):
+            save()
+            break
+        time.sleep(0.032)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-j", "--jobs", type=int, default=None)
     parser.add_argument("-b", "--blocksize", type=int, default=1024 * 1024)
     parser.add_argument("--limit", type=int, default=None)
+    parser.add_argument("--resume", type=str, default=None)
     parser.add_argument("devices", nargs="+")
     args = parser.parse_args()
 
     devices = list(args.devices)
     random.shuffle(devices)
 
-    with tqdm(unit_scale=True, unit="B", unit_divisor=1024, dynamic_ncols=True) as pbar:
-        state = State(
-            pbar=pbar,
-            blocksize=args.blocksize,
-            limit=args.limit,
-        )
+    state = State(
+        pbar=None,
+        blocksize=args.blocksize,
+        limit=args.limit,
+        devices={},
+        save_path=Path(args.resume) if args.resume else None,
+    )
+
+    if state.save_path:
+        try:
+            devset = set(devices)
+            with open(state.save_path) as f:
+                j = json.load(f)
+                state.devices = {path: Device(**d) for path, d in j["devices"].items() if path in devset}
+        except FileNotFoundError:
+            pass
+
+    initial = sum(dev.read + dev.wrote for dev in state.devices.values())
+    with tqdm(unit_scale=True, unit="B", unit_divisor=1024, dynamic_ncols=True, initial=initial) as pbar:
+        state.pbar = pbar
 
         with ThreadPoolExecutor(max_workers=args.jobs) as pool:
             for _ in pool.map(device_init, args.devices, [state] * len(args.devices)):
                 pass
-
-            for _ in pool.map(device_work, args.devices, [state] * len(args.devices)):
+            pool.submit(refresh_loop, state)
+            for _ in pool.map(device_work, state.devices.values(), [state] * len(args.devices)):
                 pass
 
 if __name__ == "__main__":
